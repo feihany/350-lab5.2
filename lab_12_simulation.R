3447b9b7b972697adda5e2fb11baf1e3fd9b6ecd
Feihan Yuan <feihany@Feihans-MBP.wv.cc.cmu.edu>
2018-08-03 13:23:45 -0400

    question 2a

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -4,12 +4,3 @@ generate_data = function(n, p){
   return(list(covariates = n-by-p, responses = r))
 }
 
-model_select = function(covariates, responses, cutoff){
-  regression = lm(responses ~ covariates)
-  new.cov = covariates[which(coef(summary(regression))[-1, 4] <= cutoff)]
-  if (length(new.cov) == 0) {return(vector(length = 0))}
-  else{
-    regression.new = lm(responses ~ new.cov)
-    return(coef(summary(regression.new))[-1, 4])
-  }
-}
