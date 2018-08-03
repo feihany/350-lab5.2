ccbabf30e5946f0316b91ebd94105754c19105fb
Feihan Yuan <feihany@Feihans-MBP.wv.cc.cmu.edu>
2018-08-03 15:25:43 -0400

    question 2c

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -13,3 +13,27 @@ model_select = function(covariates, responses, cutoff){
     return(coef(summary(regression.new))[, 4])
   }
 }
+
+run_simulation = function(n_trials = 1000, n, p, cutoff = 0.05){
+  p_values = replicate(n_trials, {model_select(generate_data(n, p)$covariates, 
+                                               generate_data(n, p)$responses, 
+                                               cutoff = cutoff)})
+  
+  jpeg(file = paste0("hist_pvalues", n, "_", p, ".jpeg"))
+  hist(unlist(p_values))
+  dev.off()
+}
+
+
+
+n = c(100, 1000, 10000)
+p = c(10, 20, 50)
+n_trials = 1000
+cutoff = 0.05
+
+comb = expand.grid(n, p)
+names(comb) = c("n", "p")
+par.mfrow = c(3, 3)
+for(i in 1:nrow(comb)){
+  run_simulation(n_trials, comb[i, 1], comb[i, 2], cutoff = 0.05)
+}
